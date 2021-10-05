cb986dd04a131bacc5f730f9ea66ca01b550f6b8
devinpickle <devinpicklesimer123@gmail.com>
2021-10-05 11:46:25 -0600

    format lastmodified, added motto

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -1,7 +1,22 @@
 const year = new Date().getFullYear();
-const lastModified = document.lastModified;
+const lastModified = new Date(document.lastModified);
+
+// Get the full name of day and month
+var weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday",
+"Thursday", "Friday", "Saturday");
+var months = new Array("January", "February", "March",
+"April", "May", "June", "July", "August", "September",
+ "October", "November", "December");
+var day = lastModified.getDay();
+var day = weekday[day];
+var month = lastModified.getMonth();
+var month = months[month];
+
+// Format for last modified in the footer
+var lastUpdated = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
+
 document.getElementById("currentyear").textContent = year;
-document.getElementById("lastModified").textContent = lastModified;
+document.getElementById("lastModified").textContent = lastUpdated;
 
 // Pressing the menu button switches the style to display
 // list items or hide them in the nav
